21d973db1c68eb4a8b215ba056af03c2267d2b50
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2022-08-22 11:49:29 -0300

    Ajustando a estrutura de rotas com links

---
+++ pf0670/aula05/src/routes/MainRoutes.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from "../components/home/Home"
+import Pagina1 from "../components/pagina1/Pagina1"
+import Pagina2 from "../components/pagina2/Pagina2"
+
+export default function MainRoutes() {
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/pag1" element={<Pagina1 />} />
+                <Route path="/pag2" element={<Pagina2 />} />
+            </Routes>
+        </>
+    )
+}
