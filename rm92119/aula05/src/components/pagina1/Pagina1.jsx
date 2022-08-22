ece4f6913862180fde680c54bf5e00311cc22d83
JoaoSorriso <rm92119@fiap.com.br>
2022-08-22 11:20:15 -0300

    Estrutura de rotas

---
+++ rm92119/aula05/src/components/pagina1/Pagina1.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function pagina1() {
+  return( 
+    <div>Pagina1</div>
+  )
+}
---
+++ rm92119/aula05/src/components/pagina2/Pagina2.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function pagina2() {
+  return(
+    <div>Pagina2</div>
+  )
+}
+
