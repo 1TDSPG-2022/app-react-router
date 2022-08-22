f3031848b13216cdc2469b414f422b30eb14f30a
lelemaia <rm94222@fiap.com.br>
2022-08-22 11:47:49 -0300

    sdv

--- rm94222/app-react-router/src/App.jsx
+++ rm94222/app-react-router/src/App.jsx
@@ -1,22 +1,21 @@
 import React from "react"
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from "./components/home/Home"
-import Pagina1 from './components/pagina1/Página1'
-import Pagina2 from './components/pagina2/Pagina2'
+import {Link} from "react-router-dom"
+import MainRoutes from "./routes/MainRoutes"
 
 
 export default function App(){
     return (
         <div>
             <h1>ROTAS</h1>
-            {/*Estrutura das rotas*/}
-            <BrowserRouter>
-               <Routes>
-                   <Route path="/" element={<Home />} />
-                   <Route path="/pag1" element={<Pagina1 />} />
-                   <Route path="/pag2" element={<Pagina2 />} />
-               </Routes>
-            </BrowserRouter>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/pag1">Pagina -1</Link></li>
+                <li><Link to="/pag2">Pagina -2s</Link></li>
+
+                <MainRoutes></MainRoutes>   
+                
+            </ul>
+           
         </div>
     )
 }
--- rm94222/app-react-router/src/index.js
+++ rm94222/app-react-router/src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App'
+import {BrowserRouter } from 'react-router-dom'
 
 
-ShadowRoot.render(<App/>, document.getElementById("root"))
+ShadowRoot.render(
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+, document.getElementById("root"))
 
  
 
