02d763e251376ad24fecfa07fe7b2619254e5ffc
Luis <rm93025@fiap.com.br>
2022-08-22 11:50:25 -0300

    Adicionados routes e paginas 1 e 2.

---
+++ rm93205/rm93205/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import {Link} from 'react-router-dom'
+import MainRoutes from "./routes/MainRoutes"
+
+export default function App(){
+    return(
+        <div>
+            <h1>ROTAS</h1>
+            <ul>
+                <li><Link to="/"/>Home</li>
+                <li><Link to="/pag1"/>Pagina-1</li>
+                <li><Link to="/pag2"/>Pagina-2</li>
+            </ul>
+            <MainRoutes/>          
+        </div>
+    )
+}
---
+++ rm93205/rm93205/src/components/pagina1/Pagina1.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function Pagina1() {
+  return <div>Pagina1</div>;
+}
+
