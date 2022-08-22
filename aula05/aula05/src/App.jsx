5e30aabf3d8da70451a4783fb2a04022e5f7606b
isabellapiola1 <rm93960@fiap.com.br>
2022-08-22 18:36:53 -0300

    ajustando a estrutura de rotas com links

--- aula05/aula05/src/App.jsx
+++ aula05/aula05/src/App.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom"
+import MainRoutes from "./routes/mainRoutes";
 
 export default function App(){
     return (
         <div>
             <h1>NOTAS</h1>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/pag1">Pagina1 - 1</Link></li>
+                <li><Link to="/pag2">Pagina1 - 2</Link></li>
+            </ul>
+            {/* Estrutura das rotas */}
+                <MainRoutes/>
+             {/* Estrutura das rotas */}    
         </div>
     )
 }
+{/* inicia p bloco das rotas */} 
+{/* gerencia as rotas */}
+{/* Route, cria a rota */}
+{/* path, atributo que associa o path ao componente */}
+{/* Home, componente que sera carregado quando a rota for carregada */}
