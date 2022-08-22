1dcf2a5f1500f3624691e00423916891bf58cc61
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2022-08-22 11:19:44 -0300

    Estrutura das rotas

--- pf0670/aula05/src/App.jsx
+++ pf0670/aula05/src/App.jsx
@@ -1,9 +1,22 @@
 import React from "react"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from "./components/home/Home"
+import Pagina1 from "./components/pagina1/Pagina1"
+import Pagina2 from "./components/pagina2/Pagina2"
 
 export default function App() {
     return (
         <div>
             <h1>ROTAS</h1>
+            {/* Estrutura das rotas */}
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/pag1" element={<Pagina1 />} />
+                    <Route path="/pag2" element={<Pagina2 />} />
+                </Routes>
+            </BrowserRouter>
+            {/* Estrutura das rotas */}
         </div>
     )
 }
