0c6f0b285d6dcded3556266fa9fc8c5d0635b4b9
FlipOwz <rm95122@fiap.com.br>
2022-08-26 08:19:50 -0300

    react criado

---
+++ rm95122/router/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import {Link} from 'react-router-dom'
+import MainRoutes from "./routes/MainRoutes"
+
+export default function App() {
+    return (
+        <div>
+            <h1>ROTAS</h1>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/pag1">Página - 1</Link></li>
+                <li><Link to="/pag2">Página - 2</Link></li>
+            </ul>
+            {/*Estrutura das rotas */}
+            <MainRoutes />
+            {/*Estrutura das rotas */}
+        </div>
+    )
+}
---
+++ rm95122/router/src/components/pagina2/Pagina2.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Pagina2() {
+    return (
+        <div>Página 2</div>
+    )
+}
