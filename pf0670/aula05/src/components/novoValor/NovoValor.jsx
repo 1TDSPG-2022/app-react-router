58e291be466c2604f36f78b78a5c4a36a5331b05
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2022-08-26 09:46:49 -0300

    Exemplo destructuring

--- pf0670/aula05/src/components/novoValor/NovoValor.jsx
+++ pf0670/aula05/src/components/novoValor/NovoValor.jsx
@@ -5,6 +5,26 @@ export default function NovoValor(){
 
     const [frase,setFrase] = useState('')
 
+    //const vetor1 = ["maçã","banana","laranja"]
+
+    //Acessando tradicionalmente
+    //console.log(vetor1[1])
+
+    //Aplicando o destructuring
+    //const { laranja } = vetor1
+    //console.log(laranja)
+
+    // const pessoa = {
+    //     nome : "Alê",
+    //     idade : 46
+    // }
+
+    // console.log(pessoa.nome)
+
+    // const{ nome } = pessoa
+    // console.log(nome)
+
+
     return(
         <div>
             <h1>Vamos escrever uma frase.</h1>
