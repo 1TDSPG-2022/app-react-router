0e588f790e46913848bd7f666e2259bad6999ab4
caiogbarreira <rm94526@fiap.com.br>
2022-08-26 08:19:56 -0300

    *first

--- rm94526/aula05/src/App.jsx
+++ rm94526/aula05/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/home/Home';
 
 export default function App() {
   return (
     <div>
       <h1>ROTAS</h1>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
