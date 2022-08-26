26cea8e399acc78ba02f61e1ffa9afe97bafbfbc
Exxtacsy <RM97068@fiap.com.br>
2022-08-26 09:46:02 -0300

    exemplo

--- aula05/aula05/src/components/novoValor/NovoValor.jsx
+++ aula05/aula05/src/components/novoValor/NovoValor.jsx
@@ -5,6 +5,25 @@ export default function NovoValor() {
 
     const [frase,setFrase] = useState('')
 
+    //const vetor1 = ["maçã", "banana", "laranja"]
+
+    //Acessando tradicionalmente
+    //console.log(vetor1[1])
+
+    //Aplicando o destructuring
+    //const { laranja } = vetor1
+    //console.log(laranja)
+
+    //const pessoa = {
+        //nome : "Isa",
+        //idade : 19
+    //}
+
+    //console.log(pessoa.nome)
+
+    //const{nome} = pessoa
+    //console.log(nome)
+
   return (
     <div>
         <h1>vamos escrever uma frase.</h1>
